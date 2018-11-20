b1d531498d41600be9c4129cbfb546b057bf5127
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-11-20 02:49:48 +0000

    Expand comments

--- src/selective_intf.ml
+++ src/selective_intf.ml
@@ -8,6 +8,15 @@ module type Basic = sig
       [First a]. Otherwise, you can skip the function and associted effects and
       return the [b] from [Second b].
 
+      Note that it is not a requirement for selective functors to skip
+      unnecessary effects. It may be counterintuitive, but this makes them more
+      useful. Why? Typically, when executing a selective computation, you would
+      want to skip the effects (saving work); but on the other hand, if your
+      goal is to statically analyse a given selective computation and extract
+      the set of all possible effects (without actually executing them), then
+      you do not want to skip any effects, because that defeats the purpose of
+      static analysis.
+
       The type signature of [select] is reminiscent of both [<*>] and [>>=],
       and indeed a selective functor is in some sense a composition of an
       applicative functor and the [Either] monad. *)
@@ -28,7 +37,8 @@ module type S = sig
 
   (** The [branch] function is a natural generalisation of [select]: instead of
       skipping an unnecessary effect, it chooses which of the two given
-      effectful functions to apply to a given argument. *)
+      effectful functions to apply to a given argument; the other effect is
+      unnecessary. *)
   val branch : ('a, 'b) Either.t t -> ('a -> 'c) t -> ('b -> 'c) t -> 'c t
 
   (** Branch on a Boolean value, skipping unnecessary effects. *)
